bbdcbeb728d11ec35098d13b1e56a75dc5d9563c
yasin çetin <cyasinn@hotmail.com>
2021-04-01 01:23:51 +0300

    10. gün

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,7 +1,9 @@
 ﻿using Business.Abstract;
+using Core.Utilities.Results;
 using DataAccess.Abstract;
 using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
+using Entities.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,6 +20,12 @@ namespace Business.Concrete
             _productDal = productDal;
         }
 
+        public IResult Add(Product product)
+        {
+            _productDal.Add(product);
+            return new Result(true,"Ürün eklendi.");
+        }
+
         public List<Product> GetAll()
         {
             // İş kodları
@@ -31,9 +39,24 @@ namespace Business.Concrete
 
         }
 
+        public Product GetById(int productId)
+        {
+            return _productDal.Get(p => p.ProductId == productId);
+        }
+
         public List<Product> GetByUnitPrice(decimal min, decimal max)
         {
             return _productDal.GetAll(p => p.UnitPrice <= min && p.UnitPrice <= max);
+        }
+
+        public List<ProductDetailDto> GetProductDetails()
+        {
+            return _productDal.GetProductDetails();
+        }
+
+        IResult IProductService.Add(Product product)
+        {
+            throw new NotImplementedException();
         }
     }
 }
